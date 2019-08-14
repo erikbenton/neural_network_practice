69582a5fb6105e06f42a112d242257efe21bb62f
Research02 <Research02@lab.electrostandards.com>
2019-08-14 16:24:48 -0400

    double checking uploads...

--- neural_network.py
+++ neural_network.py
@@ -2,11 +2,10 @@ import numpy as np
 import math
 import matplotlib as plt
 
+
 class Network:
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-
-    
