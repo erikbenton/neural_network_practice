a3a0fe7a36e9fce0550ecd6741b26a3c182dc15f
Research02 <Research02@lab.electrostandards.com>
2019-08-15 11:05:19 -0400

    finishing basic foundations, first run 95.03%

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,4 +1,4 @@
-import cPickle
+import pickle
 import gzip
 import numpy as np
 
@@ -17,7 +17,35 @@ def load_data():
     #
     # This is a nice way to format the data here, but can be difficult to deal with training_data for the
     # backwards_propagation so the load_data_wrapper function modifies it slightly
-    f = gzip.open('../neural_network_data/neural-networks-and-deep-learning/data/mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
+    f = gzip.open('mnist.pkl.gz', 'rb')
+    training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
     f.close()
     return training_data, validation_data, test_data
+
+
+def load_data_wrapper():
+    # Return (training_data, validation_data, test_data)
+    # Based on the load_data, but a more convenient format
+    #
+    # training_data is a list of 50,000 2-tuples (x, y)
+    # x - 784 dimensional numpy ndarray containing the input image
+    # y - 10 dimensional numpy ndarray with corresponding classification, or correct digit, for x
+    #
+    # Therefore the format for the training_data and the validation_data/test_data
+    tr_d, va_d, te_d = load_data()
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_results = [vectorized_result(y) for y in tr_d[1]]
+    training_data = list(zip(training_inputs, training_results))
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = list(zip(validation_inputs, va_d[1]))
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = list(zip(test_inputs, te_d[1]))
+    return training_data, validation_data, test_data
+
+
+def vectorized_result(j):
+    # Returns a 10 dimensional unit vector with a 1 in the jth position and 0s elsewhere
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 import matplotlib as plt
+import mnist_loader
+import random
 
 
 class Network:
@@ -12,8 +14,9 @@ class Network:
         return
 
     def feed_forward(self, a):
-        for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w*a + b))
+        zipped_biases_weights = list(zip(self.biases, self.weights))
+        for b, w in zipped_biases_weights:
+            a = sigmoid(np.dot(w, a) + b)
         return a
 
     def sgd(self, training_data, epochs, mini_batch_size, learning_rate, test_data=None):
@@ -21,14 +24,14 @@ class Network:
             n_test = len(test_data)
         n = len(training_data)
         for j in range(epochs):
-            np.random.shuffle(training_data)
+            random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, learning_rate)
-                if test_data:
-                    print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
-                else:
-                    print("Epoch {0} complete".format(j))
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
         return
 
     def update_mini_batch(self, mini_batch, learning_rate):
@@ -37,10 +40,14 @@ class Network:
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backwards_propagation(x, y)
-            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.biases = [b - (learning_rate/len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
-        self.weights = [w - (learning_rate / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+            zipped_nablas_b = list(zip(nabla_b, delta_nabla_b))
+            zipped_nablas_w = list(zip(nabla_w, delta_nabla_w))
+            nabla_b = [nb + dnb for nb, dnb in zipped_nablas_b]
+            nabla_w = [nw + dnw for nw, dnw in zipped_nablas_w]
+        zipped_biases_nabla_b = list(zip(self.biases, nabla_b))
+        zipped_weights_nabla_w = list(zip(self.weights, nabla_w))
+        self.biases = [b - (learning_rate/len(mini_batch)) * nb for b, nb in zipped_biases_nabla_b]
+        self.weights = [w - (learning_rate / len(mini_batch)) * nw for w, nw in zipped_weights_nabla_w]
         return
 
     def backwards_propagation(self, x, y):
@@ -52,6 +59,7 @@ class Network:
         activations = [x]
         # Layer by layer list to store all the z vectors
         zs = []
+        zipped_biases_weights = list(zip(self.biases, self.weights))
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation) + b
             zs.append(z)
@@ -66,7 +74,7 @@ class Network:
             sp = sigmoid_prime(z)
             delta = np.dot(self.weights[-i+1].transpose(), delta) * sp
             nabla_b[-i] = delta
-            nabla_w[-i] = np.dot(delta, activations[i-1].transpose())
+            nabla_w[-i] = np.dot(delta, activations[-i-1].transpose())
         return nabla_b, nabla_w
 
     def evaluate(self, test_data):
@@ -78,9 +86,14 @@ def sigmoid(z):
     return 1.0 / (1 + np.exp(-z))
 
 
-def cost_derivative( output_activations, y):
+def cost_derivative(output_activations, y):
     return output_activations - y
 
 
 def sigmoid_prime(z):
     return sigmoid(z) * (1 - sigmoid(z))
+
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = Network([784, 30, 10])
+net.sgd(training_data, 30, 10, 3.0, test_data=test_data)
